2bbf8946ae49f013fa01384693fc0bc22497619e
Irvin Steve Cardenas <irvsteve@gmail.com>
2014-11-20 01:50:44 -0500

    DigitalDoodleComplete

---
+++ app/src/main/java/edu/fiu/acm/digitaldoodlecomplete/tools/Box.java
@@ -0,0 +1,26 @@
+package edu.fiu.acm.digitaldoodlecomplete.tools;
+
+/**
+ * Created by irvin on 11/20/2014.
+ */
+import android.graphics.PointF;
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
